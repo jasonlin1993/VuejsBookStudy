2260f2fdd9f8fe1afc3e51efd47f0e68b04f0b04
jasonlin1993 <ninjachen.lin@gmail.com>
2025-01-22 14:36:46 +0800

    chapter1

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const message = ref<string>('Hello Vue 3')
-const count = ref<number>(0)
+const price = ref<number>(100)
+const quantity = ref<number>(10)
+const subtotal = computed(() => {
+  return price.value * quantity.value
+})
 </script>
 
 <template>
-  <div class="wrapper"></div>
+  <div>
+    <h1>單價: {{ price }}</h1>
+    <h1>數量:{{ quantity }}</h1>
+    <h1>總金額共{{ subtotal }}</h1>
+  </div>
 </template>
